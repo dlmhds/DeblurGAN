1b02113db95a0b58400febf77c1f02aa6cec2529
dlmhds <59540352+dlmhds@users.noreply.github.com>
2023-06-13 13:17:06 -0400

    Update train.py
    custom params

--- train.py
+++ train.py
@@ -55,12 +55,14 @@ if __name__ == '__main__':
 	# python train.py --dataroot /.path_to_your_data --learn_residual --resize_or_crop crop --fineSize CROP_SIZE (we used 256)
 
 	opt = TrainOptions().parse()
-	opt.dataroot = 'D:\Photos\TrainingData\BlurredSharp\combined'
+	opt.input_nc = 1
+	opt.output_nc = 1
+	opt.no_flip = True
 	opt.learn_residual = True
 	opt.resize_or_crop = "crop"
 	opt.fineSize = 256
 	opt.gan_type = "gan"
-	# opt.which_model_netG = "unet_256"
+	opt.which_model_netG = "resnet_9blocks"
 
 	# default = 5000
 	opt.save_latest_freq = 100
